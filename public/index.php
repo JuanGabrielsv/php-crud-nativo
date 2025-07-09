55f89629bf0474fa720785df8e2d9dd778be796d
Juangabrielsv <juangabrielsv@gmail.com>
2025-07-09 15:11:43 +0200

    Cambio de nombre de la carpeta controladores -> controllers

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 // Autoload generado por Composer
 require __DIR__ . '/../vendor/autoload.php';
 
-use App\Controladores\HomeController;
+use App\Controllers\HomeController;
 
 // Instancia el controlador principal y ejecuta método index
 $controlador = new HomeController();
--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -1,5 +1,5 @@
 <?php
-namespace App\Controladores;
+namespace App\Controllers;
 
 class HomeController
 {
