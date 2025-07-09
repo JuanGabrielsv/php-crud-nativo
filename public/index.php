bd333ddc7b190f1fbd2c36415394391548039629
Juangabrielsv <juangabrielsv@gmail.com>
2025-07-09 17:04:49 +0200

    .

--- public/index.php
+++ public/index.php
@@ -4,6 +4,6 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use App\Controllers\HomeController;
 
-// Instancia el controlador principal y ejecuta método index
+// Instancia el controlador principal y ejecuta metodo index
 $controlador = new HomeController();
 $controlador->index();
